a61fbe293f89890c4d9720cc35bd844137c76fb7
MK <mk@Narys-MacBook-Pro.local>
2025-04-23 00:07:05 +0700

    feat: solution 5; solution 6

---
+++ 5.binary-addition.js
@@ -0,0 +1,7 @@
+function addBinary(a,b) {
+    const sum = a + b;
+    const resutls = sum.toString(2);
+    return resutls
+}
+
+console.log(addBinary(1,3))
---
+++ 6.print-error.js
@@ -0,0 +1,23 @@
+function printerError(s) {
+  const array = s.split('');
+
+  const errorPattern = /[n-z]/
+
+  const error = []
+
+array.forEach(element => {
+    if(errorPattern.test(element)){
+        error.push(element)
+    }
+});
+
+const arrayLength = array.length
+const errorLength = error.length
+
+const results = `${errorLength}/${arrayLength}`
+  
+    return results
+  }
+
+  const string = "aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz" //1/14
+  console.log(printerError(string))
