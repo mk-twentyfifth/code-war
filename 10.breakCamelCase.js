bea55833ecef1af8fb3e125df3125509ccf239ce
MK <mk@Narys-MacBook-Pro.local>
2025-04-30 23:33:36 +0700

    10

---
+++ 10.breakCamelCase.js
@@ -0,0 +1,20 @@
+function solution(string) {
+  const split = string.split("");
+  let results = [];
+
+  if (string == "") {
+    return "";
+  } else {
+    split.map((s) => {
+      if (s === s.toUpperCase()) {
+        return (results = results + " " + s);
+      }
+
+      results = results + s;
+    });
+  }
+
+  return results;
+}
+
+console.log(solution(""));
